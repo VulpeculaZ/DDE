a0c17569188163fb1563d23decc1b7210da88793
Ziqian Zhou <ziqian-zhou@uiowa.edu>
2014-01-27 16:56:44 -0600

    Simulation with DSIR model in DSIRsim.R Added Measles data in data folder.

---
+++ DSIRsim.R
@@ -0,0 +1,71 @@
+source("toload.R")
+source("DSIRfn.R")
+## Function to simulate DSIR
+DSIR.gen <- function(t, y, parms){
+    if(t < 0){
+        lagI <- 0.05
+    }
+    else
+        lagI <- lagvalue(t - parms["tau"], 2)
+    sint <- sin(t / parms["f"]) + 1
+    if(y[2] > 1)
+        y[2] <- 1
+    if(y[2] < 0)
+        y[2] <- 0
+    dyS <- -parms["beta"] * lagI * y[1] + (1 - y[1]) * (parms["b"] * sint + 0.1)
+    dyI <- parms["beta"] * lagI * y[1] - (parms["gamma"] + (parms["b"] * sint + 0.1)) * y[2]
+    list(c(dyS, dyI))
+}
+
+DSIR.pars <- c(2, 0.25, 0.5, 2, 1)
+names(DSIR.pars) <- c("beta", "b","gamma", "f", "tau")
+times <- seq(-DSIR.pars["tau"], 25, by = 0.1)
+yinit <- c(0.85, 0.15)
+lambda <- 1000
+sdy <- 0.02
+n <- 2
+
+yout <- dede(y = yinit, times = times, func = DSIR.gen, parms = DSIR.pars, atol = 1e-10)
+
+times0 <- knots0 <- times[times >= 0]
+times.d <- knots.d <- times[times >= 5]
+norder = 3
+nbasis.d = length(knots.d) + norder - 2
+nbasis0 <- length(knots0) + norder - 2
+range0  = range(knots0)
+range.d <- range(knots.d)
+basis0 <- create.bspline.basis(range=range(knots0), nbasis=nbasis0, norder=norder, breaks=knots0)
+basis.d <- create.bspline.basis(range=range.d, nbasis=nbasis.d, norder=norder, breaks=knots.d)
+fdnames=list(NULL,c('S', 'I'),NULL)
+bfdPar0 = fdPar(basis0,lambda=1,int2Lfd(1))
+bfdPar.d <- fdPar(basis.d,lambda=1,int2Lfd(1))
+
+
+coefs.list <- conv.list <- data.list <- list()
+par.mat <- c()
+set.seed(42)
+for (i in 1:n){
+    initPars <- DSIR.pars[c(1, 3,5)] + runif(3, -0.2, 0.2)
+    xout <- c()
+    xout <- cbind(xout, yout[,2] + rnorm(length(yout[,2]), sd = sdy))
+    xout <- cbind(xout, yout[,3] + rnorm(length(yout[,2]), sd = sdy))
+    xout[xout < 0] <- 0
+    xout0 <- xout[times >= 0,]
+    xout.d <- xout[times >= 5,]
+    DEfd0 <- smooth.basis(knots0,xout0,bfdPar0,fdnames=fdnames)$fd
+    DEfd.d <- smooth.basis(knots.d, xout.d, bfdPar.d, fdnames=fdnames)$fd
+    ## extract the coefficients and assign variable names
+    coefs0 <- DEfd0$coefs
+    colnames(coefs0) = c("S","I")
+    coefs.d <- DEfd.d$coefs
+    colnames(coefs.d) = c("S", "I")
+    dsirData <- matrix(xout.d, ncol =2, dimnames = list(NULL,c("S", "I")))
+    dde.fit <- Profile.LS.DDE(DSIRfn, dsirData, times.d, initPars, coefs = coefs.d, basisvals = basis.d, lambda, in.meth='nlminb', delay = delay, basisvals0 = basis0, coefs0 = coefs0, tauIndex = c(FALSE, TRUE))
+    DEfd2 = fd(dde.fit$coefs,basis.d, fdnames)
+    coefs.list[[i]] <- dde.fit$coefs
+    par.mat <- rbind(par.mat, c(dde.fit$pars, initPars))
+    conv.list[[i]] <- dde.fit$convInfo
+    data.list[[i]] <- dde.fit$data
+}
+
+save((DSIR.pars, coefs.List, conv.list, data.list, par.mat, file = paste(paste(n,sdy,lambda, sep="_"),".RData"))
--- vector.disease.R
+++ vector.disease.R
@@ -1,5 +1,7 @@
 library(CollocInfer)
 library(deSolve)
+nls.control(warnOnly = TRUE)
+
 
 ## Function to simulating data
 vectorD.Gen <- function(t, y, parms){
@@ -12,21 +14,15 @@ vectorD.Gen <- function(t, y, parms){
 }
 vectorPars <- c(1, 1.8,0.8)
 names(vectorPars) <- c("a","b","tau")
-
 yinit <- 0.05
 times <- seq(-0.8, 25, by = 0.1)
 ## solve the model
 
 yout <- dede(y = yinit, times = times, func = vectorD.Gen, parms = vectorPars, atol = 1e-10)
-plot(yout, which = 1, type = "l", lwd = 2, main = "Vector Disease Model")
-plot(yout[,2], yout[,3], xlab = "y", ylab = "dy", type = "l", lwd = 2)
+# plot(yout, which = 1, type = "l", lwd = 2, main = "Vector Disease Model")
+# plot(yout[,2], yout[,3], xlab = "y", ylab = "dy", type = "l", lwd = 2)
 
-xout <- yout[,2] + rnorm(length(yout[,2]), sd = 0.01)
-xout[xout < 0] <- 0
-## points(times, xout)
-xout0 <- xout[times >= 0]
 
-xout.d <- xout[times >= 5]
 times0 <- knots0 <- times[times >= 0]
 times.d <- knots.d <- times[times >= 5]
 norder = 3
@@ -40,36 +36,37 @@ range.d <- range(knots.d)
 vectorBasis0 <- create.bspline.basis(range=range(knots0), nbasis=nbasis0, norder=norder, breaks=knots0)
 vectorBasis.d <- create.bspline.basis(range=range.d, nbasis=nbasis.d, norder=norder, breaks=knots.d)
 
-
+##  Set a value for lambda
+lambda = 1000
 fdnames=list(NULL,c('S'),NULL)
 bfdPar0 = fdPar(vectorBasis0,lambda=1,int2Lfd(1))
 bfdPar.d <- fdPar(vectorBasis.d,lambda=1,int2Lfd(1))
-DEfd0 <- smooth.basis(knots0,xout0,bfdPar0,fdnames=fdnames)$fd
-DEfd0.true <- smooth.basis(knots0,yout[times >=0,2],bfdPar0,fdnames=fdnames)$fd
-DEfd.d <- smooth.basis(knots.d, xout.d, bfdPar.d, fdnames=fdnames)$fd
-temp.fit <- eval.fd(times.d, DEfd.d)
-## par(ask=FALSE)
-plotfit.fd(xout[times >=0], times0, DEfd0)
-plotfit.fd(xout[times >=5], times.d, DEfd.d)
 
+## Setting initial values
+vectorPars <- c(0.8, 1.5,0.9)
+names(vectorPars) <- c("a","b","tau")
+
+
+xout <- yout[,2] + rnorm(length(yout[,2]), sd = 0.01)
+xout[xout < 0] <- 0
+xout0 <- xout[times >= 0]
+xout.d <- xout[times >= 5]
+DEfd0 <- smooth.basis(knots0,xout0,bfdPar0,fdnames=fdnames)$fd
+DEfd.d <- smooth.basis(knots.d, xout.d, bfdPar.d, fdnames=fdnames)$fd
+## temp.fit <- eval.fd(times.d, DEfd.d)
+## par(ask=FALSE)
+## plotfit.fd(xout[times >=0], times0, DEfd0)
+## plotfit.fd(xout[times >=5], times.d, DEfd.d)
 ## extract the coefficients and assign variable names
 coefs0 <- DEfd0$coefs
 colnames(coefs0) = "S"
 coefs.d <- DEfd.d$coefs
 colnames(coefs.d) = "S"
-
-##  Set a value for lambda
-lambda = 1000
-
 ## Data
 vectorData <- matrix(xout.d, ncol =1, dimnames = list(NULL,"S"))
 
-## Setting initial values
-vectorPars <- c(0.8, 1.5,0.8)
-names(vectorPars) <- c("a","b","tau")
 
 ## debug(Smooth.LS.DDE)
-nls.control(warnOnly = TRUE)
 dde.1fit2 <- Smooth.LS.DDE(vectorFun, vectorData, times.d, vectorPars, coefs = coefs.d, coefs0, basisvals = vectorBasis.d, basisvals0 =  vectorBasis0, lambda, in.meth='nlminb', delay = delay)
 # Let's have a look at this
 coefs1 = dde.1fit2$coefs
@@ -78,74 +75,55 @@ plotfit.fd(vectorData,times.d , DEfd1)
 
 ## debug(Profile.LS.DDE)
 # Let's have a look at this
-init.list <- list()
+
+dde.fit <- Profile.LS.DDE(vectorFun, vectorData, times.d, vectorPars, coefs = coefs1, basisvals = vectorBasis.d,  lambda, in.meth='nlminb', delay = delay, basisvals0 = vectorBasis0, coefs0 = coefs0, tauIndex = c(FALSE, FALSE, TRUE))
+DEfd2 = fd(dde.fit$coefs,vectorBasis.d, fdnames)
+plotfit.fd(vectorData,times.d , DEfd2)
+
+
+
+# init.list <- list()
 fit.list <- list()
 par.list <- list()
+set.seed(42)
 for (i in 1:20){
-    vectorPars.i <- vectorPars + 0.1 * (0.5 - runif(3))
-    fit.list[[i]] <- vectorPars.i
-    dde.fit <- Profile.LS.DDE(vectorFun, vectorData, times.d, vectorPars, coefs = coefs1, basisvals = vectorBasis.d,  lambda, in.meth='nlminb', delay = delay, basisvals0 = vectorBasis0, coefs0 = coefs0, tauIndex = c(FALSE, FALSE, TRUE))
+    xout <- yout[,2] + rnorm(length(yout[,2]), sd = 0.01)
+    xout[xout < 0] <- 0
+    xout0 <- xout[times >= 0]
+    xout.d <- xout[times >= 5]
+    DEfd0 <- smooth.basis(knots0,xout0,bfdPar0,fdnames=fdnames)$fd
+    DEfd.d <- smooth.basis(knots.d, xout.d, bfdPar.d, fdnames=fdnames)$fd
+    ## extract the coefficients and assign variable names
+    coefs0 <- DEfd0$coefs
+    colnames(coefs0) = "S"
+    coefs.d <- DEfd.d$coefs
+    colnames(coefs.d) = "S"
+    ## Data
+    vectorData <- matrix(xout.d, ncol =1, dimnames = list(NULL,"S"))
+    dde.fit <- Profile.LS.DDE(vectorFun, vectorData, times.d, vectorPars, coefs = coefs.d, basisvals = vectorBasis.d,  lambda, in.meth='nlminb', delay = delay, basisvals0 = vectorBasis0, coefs0 = coefs0, tauIndex = c(TRUE))
     DEfd2 = fd(dde.fit$coefs,vectorBasis.d, fdnames)
     fit.list[[i]] <- DEfd2
     par.list[[i]] <- dde.fit$pars
 }
 
-DEfd2 = fd(dde.fit$coefs,vectorBasis.d, fdnames)
-plotfit.fd(vectorData,times.d , DEfd2)
+xout <- yout[,2] + rnorm(length(yout[,2]), sd = 0.01)
+xout[xout < 0] <- 0
+xout0 <- xout[times >= 0]
+xout.d <- xout[times >= 5]
+DEfd0 <- smooth.basis(knots0,xout0,bfdPar0,fdnames=fdnames)$fd
+DEfd.d <- smooth.basis(knots.d, xout.d, bfdPar.d, fdnames=fdnames)$fd
+## temp.fit <- eval.fd(times.d, DEfd.d)
+## par(ask=FALSE)
+## plotfit.fd(xout[times >=0], times0, DEfd0)
+## plotfit.fd(xout[times >=5], times.d, DEfd.d)
+## extract the coefficients and assign variable names
+coefs0 <- DEfd0$coefs
+colnames(coefs0) = "S"
+coefs.d <- DEfd.d$coefs
+colnames(coefs.d) = "S"
+## Data
+vectorData <- matrix(xout.d, ncol =1, dimnames = list(NULL,"S"))
 
-
-save(list = ls(), file = "nls1.Rdata")
-## Testing a intermediate ProfileSSE.Allpar.DDE
-load("nls1.Rdata")
-tmpfd <- fd(ncoefs, basisvals)
-plotfit.fd(data, times, tmpfd)
-    ## Calculate fitted value after inner optimization:
-    devals = as.matrix(lik$bvals%*%ncoefs)
-    colnames(devals) = proc$more$names
-
-    ## Squared errors: No need to change for DDE
-    weights = checkweights(lik$more$weights,lik$more$whichobs,data)
-    f = as.vector( as.matrix(data - lik$more$fn(times, devals, pars, lik$more$more))*sqrt(weights))
-    isnaf = is.na(f)
-    f[isnaf] = 0
-
-    dlikdp = lik$more$dfdp(times,devals,pars,lik$more$more)
-    dlikdp = matrix(dlikdp,dim(dlikdp)[1]*dim(dlikdp)[2],dim(dlikdp)[3])
-    ## dlikdp will be zero if the likelihood doesn't directly have ode parameters,
-    ## which is true for least square case.
-    dlikdx = lik$more$dfdx(times,devals,pars,lik$more$more)
-    ## dlikdx[i,,] is an identity matrix for every i.
-
-    dlikdc = c()
-    for(i in 1:dim(dlikdx)[2]){
-        tH = c()
-        for(j in 1:dim(dlikdx)[3]){
-            ## Only dlikdx[,i,i] are non-zero (all 1's)
-            tH = cbind(tH,as.matrix(diag(dlikdx[,i,j])%*%lik$bvals))
-        }
-        dlikdc = rbind(dlikdc,tH)
-    }
-
-    ## ??dlikdc: why 0.5 and 0.5 ??
-    d2Hdc2  = SplineCoefsDC2sparse(ncoefs,times,data,lik,proc,pars)
-    ## need not be changed?
-    d2Hdcdp = SplineCoefsDCDP(ncoefs,times,data,lik,proc,pars)
-    ## Got warning message:
-    ## In dim(weights[whichrows, ]) == dim(diffs):
-    ## longer object length is not a multiple of shorter object length?
-
-    ## Use Implicit function theorem:
-    if(is.matrix(d2Hdc2)){
-        ## When will it not be a matrix? How to use solve in that case?
-        dcdp = ginv(d2Hdc2) %*% d2Hdcdp
-    } else {
-        dcdp = as.matrix(solve(d2Hdc2,d2Hdcdp))
-    }
-
-    ## Chain rule:
-    df = dlikdc%*%dcdp + dlikdp
-    df[isnaf,] = 0
-    colnames(df) = proc$more$parnames
 
 
 
@@ -217,13 +195,6 @@ temp.d2dfdx2 <- function(data, times, devals, pars, more){
     return(2 * H)
 }
 
-## debugging in: SplineCoefsDC2sparse(coefs, times, data, lik, proc, pars, sgn):
-debug(proc$d2fdc2)
-temp <- proc$d2fdc2(coefs2, proc$bvals, pars, proc$more)
-
-##
-debug(temp.d2dfdx2)
-H1 = temp.d2dfdx2(ddevals, more$qpts, devals, pars, more)
 
 collocNS <- getNamespace("CollocInfer")
 unlockBinding("checkweights", collocNS)
